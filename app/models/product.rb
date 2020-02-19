57e1fd23b23e8fe5605f7285c651530888ad0deb
akiosan0803 <wisdom.glory.passion.2030@gmail.com>
2020-02-19 23:23:06 +0900

    commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :iine_users, through: :likes, source: :user
   has_one_attached :image
-  validates :image, file_size: { in: 100.kilobytes..1.megabyte }, file_content_type: { allow: ['image/jpeg', 'image/png'] }
+  validates :image, file_size: { in: 10.kilobytes..1.megabyte }, file_content_type: { allow: ['image/jpeg', 'image/png'] }
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)
