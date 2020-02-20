32e8890e16ddeace27631b1ded57a614f3cd2064
akiosan0803 <wisdom.glory.passion.2030@gmail.com>
2020-02-20 22:42:21 +0900

    commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,13 @@ class LikesController < ApplicationController
 
   def create
     like = Like.new(user_id: current_user.id, product_id: params[:product_id])
-    if like.save
-      redirect_to product_path(params[:product_id])
-    else
-      redirect_to product_path(params[:product_id])
-    end
+    like.save
+    redirect_to product_path(params[:product_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
     like.destroy
-    redirect_to product_path(params[:product_id]) 
+    redirect_to product_path(params[:product_id])
   end 
 end
